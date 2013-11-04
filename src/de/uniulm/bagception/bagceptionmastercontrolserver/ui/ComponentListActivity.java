de59ccd8c629da3b6c995897c50d7d8b1eac02c2
Philipp Hock <mail@philipphock.de>
2013-11-04 21:02:13 +0100

    service list updated

--- src/de/uniulm/bagception/bagceptionmastercontrolserver/ui/ComponentListActivity.java
+++ src/de/uniulm/bagception/bagceptionmastercontrolserver/ui/ComponentListActivity.java
@@ -6,7 +6,6 @@ import android.app.Fragment;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
-import de.philipphock.android.lib.logging.LOG;
 import de.uniulm.bagception.bagceptionmastercontrolserver.R;
 import de.uniulm.bagception.bagceptionmastercontrolserver.listContent.ListContent;
 
--- src/de/uniulm/bagception/bagceptionmastercontrolserver/ui/fragments/ServiceStatusFragment.java
+++ src/de/uniulm/bagception/bagceptionmastercontrolserver/ui/fragments/ServiceStatusFragment.java
@@ -16,7 +16,11 @@ public class ServiceStatusFragment extends ListFragment {
 		
 		
 		ServiceInfoArrayAdapter adapter = new ServiceInfoArrayAdapter(getActivity());
+		adapter.add(new ServiceInfo(getActivity(),adapter,"MasterControlService"));
 		adapter.add(new ServiceInfo(getActivity(),adapter,"MiniMe API Service"));
+		adapter.add(new ServiceInfo(getActivity(),adapter,"Bluetooth Service"));
+		adapter.add(new ServiceInfo(getActivity(),adapter,"Case Open Service"));
+		
 		setListAdapter(adapter);
 
 	}
@@ -29,7 +33,5 @@ public class ServiceStatusFragment extends ListFragment {
 //
 //	}
 
-	private void updateServiceStatus() {
 
-	}
 }
