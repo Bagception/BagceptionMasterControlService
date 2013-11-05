156a2bfd9fe598faf62ce092858a9749c3695b5e
Philipp Hock <mail@philipphock.de>
2013-11-05 13:55:50 +0100

    minime rfid service added

--- src/de/uniulm/bagception/bagceptionmastercontrolserver/ui/fragments/ServiceStatusFragment.java
+++ src/de/uniulm/bagception/bagceptionmastercontrolserver/ui/fragments/ServiceStatusFragment.java
@@ -19,8 +19,7 @@ public class ServiceStatusFragment extends ListFragment {
 		
 		
 //		adapter.add(new ServiceInfo(getActivity(),adapter,"MasterControlService"));
-//		adapter.add(new ServiceInfo(getActivity(),adapter,"MiniMe API Service"));
-//		adapter.add(new ServiceInfo(getActivity(),adapter,"Bluetooth Service"));
+		adapter.add(new ServiceInfo(getActivity(),adapter,"RFID Service",ServiceNames.RFID_SERVICE));
 		adapter.add(new ServiceInfo(getActivity(),adapter,"Case Open Service",ServiceNames.CASE_OPEN_SERVICE));
 		adapter.add(new ServiceInfo(getActivity(),adapter,"Bluetooth Service",ServiceNames.BLUETOOTH_SERVER_SERVICE));
 		
