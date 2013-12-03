69c77d6b8acdf9c731c444bfe8782646128d391b
Steffen Herrdum <steffen.herrdum@uni-ulm.de>
2013-12-03 16:21:54 +0100

    geht nicht

--- src/de/uniulm/bagception/bagceptionmastercontrolserver/database/TestClass.java
+++ src/de/uniulm/bagception/bagceptionmastercontrolserver/database/TestClass.java
@@ -1,11 +1,24 @@
 package de.uniulm.bagception.bagceptionmastercontrolserver.database;
 
-public class TestClass {
+import android.content.Intent;
+import android.os.Parcel;
+import android.os.Parcelable;
+
+public class TestClass implements Parcelable.Creator<Item>{
 	
-	private Item item;
 	
 	public TestClass(){
-		
+	}
+
+	@Override
+	public Item createFromParcel(Parcel source) {
+		return new Item(source);
+	}
+
+	@Override
+	public Item[] newArray(int size) {
+		// TODO Auto-generated method stub
+		return new Item[size];
 	}
 
 }
