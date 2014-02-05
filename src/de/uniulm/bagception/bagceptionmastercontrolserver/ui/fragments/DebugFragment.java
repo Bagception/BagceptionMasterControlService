aeaccb7628f3628b01c85f733018baf898804a09
Steffen Herrdum <steffen.herrdum@uni-ulm.de>
2014-02-05 13:55:27 +0100

    added location service

--- src/de/uniulm/bagception/bagceptionmastercontrolserver/ui/fragments/DebugFragment.java
+++ src/de/uniulm/bagception/bagceptionmastercontrolserver/ui/fragments/DebugFragment.java
@@ -1,15 +1,22 @@
 package de.uniulm.bagception.bagceptionmastercontrolserver.ui.fragments;
 
 import android.app.Fragment;
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.ViewGroup;
 import android.widget.Button;
 import de.uniulm.bagception.bagceptionmastercontrolserver.R;
+import de.uniulm.bagception.bagceptionmastercontrolserver.service.location.LocationService;
 import de.uniulm.bagception.bagceptionmastercontrolserver.ui.log_fragment.LOGGER;
+import de.uniulm.bagception.intentservicecommunication.MyResultReceiver;
+import de.uniulm.bagception.intentservicecommunication.MyResultReceiver.Receiver;
 import de.uniulm.bagception.mcs.services.MasterControlServer;
+import de.uniulm.bagception.services.attributes.OurLocation;
 
 /**
  * Part of the multi-pane view<br>
@@ -17,7 +24,9 @@ import de.uniulm.bagception.mcs.services.MasterControlServer;
  * @author phil
  *
  */
-public class DebugFragment extends Fragment{
+public class DebugFragment extends Fragment implements Receiver{
+	
+	private MyResultReceiver mResultreceiver;
 
 
 	@Override
@@ -34,11 +43,41 @@ public class DebugFragment extends Fragment{
 				
 			}
 		});
+		
+		mResultreceiver = new MyResultReceiver(new Handler());
+		mResultreceiver.setReceiver(this);
 
 		
 
+		Button testLocation = (Button) v.findViewById(R.id.testlocation);
+		testLocation.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				Intent i = new Intent(getActivity(),LocationService.class);
+				i.putExtra("receiverTag", mResultreceiver);
+				i.putExtra(OurLocation.REQUEST_TYPE, OurLocation.GETLOCATION);
+				getActivity().startService(i);
+			}
+		});
+		
 		return v;
 	}
+
+	@Override
+	public void onReceiveResult(int resultCode, Bundle resultData) {
+		log("answer received!");
+		if(resultData.containsKey(OurLocation.RESPONSE_TYPE)){
+			if(resultData.getString(OurLocation.RESPONSE_TYPE).equals(OurLocation.LOCATION)){
+				log("it works..." + resultData.getString(OurLocation.REQUEST_TYPE));
+			}
+		}
+	}
+	
+	private void log(String s){
+		Log.d("MCS", s);
+	}
+
 
 	
 
