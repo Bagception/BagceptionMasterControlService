4a0d409ebe47503ad582822a10577f2fa3073f09
ms122 <michael.schiller@uni-ulm.de>
2014-02-11 12:45:46 +0100

    Update database

--- src/de/uniulm/bagception/bagceptionmastercontrolserver/database/Utility.java
+++ src/de/uniulm/bagception/bagceptionmastercontrolserver/database/Utility.java
@@ -9,13 +9,17 @@ public class Utility {
 	
 	 // convert from bitmap to byte array
 	 public static byte[] getBytes(Bitmap bitmap) {
-		  ByteArrayOutputStream stream = new ByteArrayOutputStream();
-		  bitmap.compress(CompressFormat.PNG, 0, stream);
-		  return stream.toByteArray();
+		 if(bitmap != null){
+			  ByteArrayOutputStream stream = new ByteArrayOutputStream();
+			  bitmap.compress(CompressFormat.PNG, 0, stream);
+			  return stream.toByteArray();
+		 } else return null;
 	 }
 
 	  // convert from byte array to bitmap
 	 public static Bitmap getPhoto(byte[] image) {
-		 return BitmapFactory.decodeByteArray(image, 0, image.length);
+		 if(image != null){
+			 return BitmapFactory.decodeByteArray(image, 0, image.length);
+		 } else return null;
 	 }
 }
