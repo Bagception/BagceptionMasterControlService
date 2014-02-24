6ed98d287b03d3f008ff58553fe9eb3e4fdef3ca
ms122 <michael.schiller@uni-ulm.de>
2014-02-24 18:26:56 +0100

    Bugfix Weather

--- src/de/uniulm/bagception/bagceptionmastercontrolserver/service/weatherforecast/WeatherForecastService.java
+++ src/de/uniulm/bagception/bagceptionmastercontrolserver/service/weatherforecast/WeatherForecastService.java
@@ -18,6 +18,7 @@ import android.os.AsyncTask;
 import android.os.Bundle;
 import android.os.ResultReceiver;
 import android.util.Log;
+import android.widget.Toast;
 import de.uniulm.bagception.services.attributes.WeatherForecast;
 
 public class WeatherForecastService extends IntentService {
@@ -106,36 +107,41 @@ public class WeatherForecastService extends IntentService {
 				float rain = -99f;
 				de.uniulm.bagception.bundlemessageprotocol.entities.WeatherForecast forecast = 
 						new de.uniulm.bagception.bundlemessageprotocol.entities.WeatherForecast(city, tempMax, wind, clouds, tempMin, tempMax, rain);
-				if(jsonObject.containsKey("list")){
-					log("has list element");
-					array = (JSONArray) jsonObject.get("list");
-					try {
-						JSONObject obj = (JSONObject) parser.parse(array.get(0).toString());
-//						if(obj.containsKey("name")) forecast.setCity(obj.get("name").toString());
-//						if(obj.containsKey("main")) mainObject = (JSONObject) parser.parse(obj.get("main").toString());
-//						if(mainObject.containsKey("temp")) forecast.setTemp(Float.parseFloat(mainObject.get("temp").toString()));
-//						if(mainObject.containsKey("temp_min")) forecast.setTemp_min(Float.parseFloat(mainObject.get("temp_min").toString()));
-//						if(mainObject.containsKey("temp_max")) forecast.setTemp_max(Float.parseFloat(mainObject.get("temp_max").toString()));
-//						if(obj.containsKey("wind")) windObject = (JSONObject) parser.parse(obj.get("wind").toString());
-//						if(windObject != null && windObject.containsKey("speed")) forecast.setWind(Float.parseFloat(windObject.get("speed").toString()));
-//						if(obj.containsKey("clouds")) cloudObject = (JSONObject) parser.parse(obj.get("clouds").toString());
-//						if(cloudObject != null && cloudObject.containsKey("all")) forecast.setClouds(Float.parseFloat(cloudObject.get("all").toString()));
-//						if(obj.containsKey("rain")) rainObject = (JSONObject) parser.parse(obj.get("rain").toString());
-//						if(rainObject != null && rainObject.containsKey("3h"))	forecast.setRain(Float.parseFloat(rainObject.get("3h").toString()));
-						if(obj.containsKey("name")) city =  obj.get("name").toString();
-						if(obj.containsKey("main")) mainObject = (JSONObject) parser.parse(obj.get("main").toString());
-						if(mainObject.containsKey("temp")) temp =  Float.parseFloat(mainObject.get("temp").toString());
-						if(mainObject.containsKey("temp_min")) tempMin = Float.parseFloat(mainObject.get("temp_min").toString());
-						if(mainObject.containsKey("temp_max")) tempMax = Float.parseFloat(mainObject.get("temp_max").toString());
-						if(obj.containsKey("wind")) windObject = (JSONObject) parser.parse(obj.get("wind").toString());
-						if(windObject != null && windObject.containsKey("speed")) wind = Float.parseFloat(windObject.get("speed").toString());
-						if(obj.containsKey("clouds")) cloudObject = (JSONObject) parser.parse(obj.get("clouds").toString());
-						if(cloudObject != null && cloudObject.containsKey("all")) clouds = Float.parseFloat(cloudObject.get("all").toString());
-						if(obj.containsKey("rain")) rainObject = (JSONObject) parser.parse(obj.get("rain").toString());
-						if(rainObject != null && rainObject.containsKey("3h"))	rain = Float.parseFloat(rainObject.get("3h").toString());
-					} catch (ParseException e) {
-						e.printStackTrace();
+				
+				if(jsonObject != null){
+					if(jsonObject.containsKey("list")){
+						log("has list element");
+						array = (JSONArray) jsonObject.get("list");
+						try {
+							JSONObject obj = (JSONObject) parser.parse(array.get(0).toString());
+	//						if(obj.containsKey("name")) forecast.setCity(obj.get("name").toString());
+	//						if(obj.containsKey("main")) mainObject = (JSONObject) parser.parse(obj.get("main").toString());
+	//						if(mainObject.containsKey("temp")) forecast.setTemp(Float.parseFloat(mainObject.get("temp").toString()));
+	//						if(mainObject.containsKey("temp_min")) forecast.setTemp_min(Float.parseFloat(mainObject.get("temp_min").toString()));
+	//						if(mainObject.containsKey("temp_max")) forecast.setTemp_max(Float.parseFloat(mainObject.get("temp_max").toString()));
+	//						if(obj.containsKey("wind")) windObject = (JSONObject) parser.parse(obj.get("wind").toString());
+	//						if(windObject != null && windObject.containsKey("speed")) forecast.setWind(Float.parseFloat(windObject.get("speed").toString()));
+	//						if(obj.containsKey("clouds")) cloudObject = (JSONObject) parser.parse(obj.get("clouds").toString());
+	//						if(cloudObject != null && cloudObject.containsKey("all")) forecast.setClouds(Float.parseFloat(cloudObject.get("all").toString()));
+	//						if(obj.containsKey("rain")) rainObject = (JSONObject) parser.parse(obj.get("rain").toString());
+	//						if(rainObject != null && rainObject.containsKey("3h"))	forecast.setRain(Float.parseFloat(rainObject.get("3h").toString()));
+							if(obj.containsKey("name")) city =  obj.get("name").toString();
+							if(obj.containsKey("main")) mainObject = (JSONObject) parser.parse(obj.get("main").toString());
+							if(mainObject.containsKey("temp")) temp =  Float.parseFloat(mainObject.get("temp").toString());
+							if(mainObject.containsKey("temp_min")) tempMin = Float.parseFloat(mainObject.get("temp_min").toString());
+							if(mainObject.containsKey("temp_max")) tempMax = Float.parseFloat(mainObject.get("temp_max").toString());
+							if(obj.containsKey("wind")) windObject = (JSONObject) parser.parse(obj.get("wind").toString());
+							if(windObject != null && windObject.containsKey("speed")) wind = Float.parseFloat(windObject.get("speed").toString());
+							if(obj.containsKey("clouds")) cloudObject = (JSONObject) parser.parse(obj.get("clouds").toString());
+							if(cloudObject != null && cloudObject.containsKey("all")) clouds = Float.parseFloat(cloudObject.get("all").toString());
+							if(obj.containsKey("rain")) rainObject = (JSONObject) parser.parse(obj.get("rain").toString());
+							if(rainObject != null && rainObject.containsKey("3h"))	rain = Float.parseFloat(rainObject.get("3h").toString());
+						} catch (ParseException e) {
+							e.printStackTrace();
+						}
 					}
+				} else {
+					Toast.makeText(getApplicationContext(), "Keine Wetterdaten verfügbar", 0).show();
 				}
 				
 				answer.put("city", city);
