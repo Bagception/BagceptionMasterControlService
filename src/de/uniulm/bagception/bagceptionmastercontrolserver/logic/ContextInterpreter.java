adc2b88729ec9b0a3079e7ca3aa106b9fc4ef7af
Philipp Hock <philipp.hock@uni-ulm.de>
2014-02-24 14:51:00 +0100

    fixed update context

--- src/de/uniulm/bagception/bagceptionmastercontrolserver/logic/ContextInterpreter.java
+++ src/de/uniulm/bagception/bagceptionmastercontrolserver/logic/ContextInterpreter.java
@@ -427,8 +427,8 @@ public class ContextInterpreter implements Receiver{
 //			ret.add(new CachedContextInfo(CONTEXT.BRIGHT, "hell"));
 			String c="";
 			for (CachedContextInfo cc:ret){
+				c+="\n ";
 				c+=cc.getContext().name();
-				c+="\n";
 			}
 			
 			mcs.sendMessageToRemote("Aktiver Kontext: "+c);
